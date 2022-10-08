5200b8c039090c6ce71ae855a9deb95332ab3f25
ambercc <839050651@qq.com>
2022-10-08 14:46:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# heroku twilio
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/amber6hua/twilio.git)
+
+You can deploy without cloning this repo to test it out.
+
---
+++ settings.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(verbose=True)
+
+FROM_EMAIL = os.environ.get("FROM_EMAIL")
+TO_EMAIL = os.environ.get("TO_EMAIL")
+KEY = os.environ.get("KEY")
+SMTP_SSL = os.environ.get("SMTP_SSL")
+
+if not FROM_EMAIL or not KEY:
+    raise SystemExit('FROM_EMAIL/KEY pair not set, check .env file')
